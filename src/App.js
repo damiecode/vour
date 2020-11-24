64f119b82e2992841b8fa9bb2634a2de28e28955
damiecode <codenlyn@gmail.com>
2020-11-24 19:53:39 +0100

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './homepage';
 
 function App() {
   return (
     <div>
+      <Homepage />
     </div>
   );
 }
